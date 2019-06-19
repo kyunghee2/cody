62f0adfc56f2617b71431a1c9430da57f4b7f22d
student <student@M150115>
2019-06-19 13:23:12 +0900

    CLOTH SERVICE

--- src/main/java/spring/biz/cloth/service/ClothService.java
+++ src/main/java/spring/biz/cloth/service/ClothService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import spring.biz.cloth.vo.ClothVO;
 
 public interface ClothService {
-
+	
 	int addCloth(ClothVO cloth);
 	List<ClothVO> getClothList();
 	List<ClothVO> getCloth(String uid, String kind);
