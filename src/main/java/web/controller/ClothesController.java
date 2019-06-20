125c7660981d8ab885a6f87401795676a9456d8b
jungsehwan <jungsehwan0612@gmail.com>
2019-06-20 14:35:33 +0900

    push

--- src/main/java/web/controller/ClothesController.java
+++ src/main/java/web/controller/ClothesController.java
@@ -1,14 +1,8 @@
 package web.controller;
 
-import java.io.File;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ClothesController {
