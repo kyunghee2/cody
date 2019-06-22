50d0c46ffe3033e1807cfb2bf28a415adccf36ee
jungsehwan0612 <jungsehwan0612@gmail.com>
2019-06-22 14:51:39 +0900

    Update JDBCUtil.java
    made connection to 70.12.115.75

--- src/main/java/util/JDBCUtil.java
+++ src/main/java/util/JDBCUtil.java
@@ -1,31 +1,22 @@
 package util;
 
-import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 public class JDBCUtil {
 	
 	public static Connection getConnection() {
 		
-//		String driver="oracle.jdbc.OracleDriver";
-//		String url="jdbc:oracle:thin:@127.0.0.1:1521:xe";
-//		String user ="scott";
-//		String pw="TIGER";
+		String driver="oracle.jdbc.OracleDriver";
+		String url="jdbc:oracle:thin:@70.12.115.75:1521:xe";
+		String user ="cody";
+		String pw="cody";
 		
 		Connection con=null;
 		try {
-			Properties p=new Properties();
-			p.load(new FileInputStream("c://lib/dbinfo.txt"));
-			String driver=p.getProperty("driver");
-			String url=p.getProperty("url");
-			String user =p.getProperty("user");
-			String pw=p.getProperty("pw");
-			
 			Class.forName(driver);
 			con =DriverManager.getConnection(url, user, pw);
 			
